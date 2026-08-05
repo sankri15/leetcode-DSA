ef1802e1ae94db4c8807d2aa2cf55bbbd90f7876
Sanjana Pal <sanjanapal004@gmail.com>
2026-08-05 21:38:30 +0530

    Time: 3 ms (44.2%), Space: 8.7 MB (37.23%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;  
+
+        long long rev = 0, num = x;
+        while(num) {
+            rev = rev * 10 + num % 10;  
+            num /= 10;
+        }
+        return rev == x;
+    }
+};
