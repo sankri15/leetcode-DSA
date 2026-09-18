092cd452b34039b65fd6f9be2757d81ae61efbdf
Sanjana Pal <sanjanapal004@gmail.com>
2026-09-18 17:11:42 +0530

    Time: 0 ms (100%), Space: 12.8 MB (74.4%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        for (int i = triangle.size() - 2; i >= 0; i--)
-            for (int j = 0; j < triangle[i].size(); j++)
-                triangle[i][j] +=
-                    min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        int row = triangle.size();
+        vector<int> memo = triangle[row-1];
 
-        return triangle[0][0];
+        for (int r = row-2; r >= 0; r--) {
+            for (int c = 0; c <= r; c++) {
+                memo[c] = min(memo[c], memo[c+1]) + triangle[r][c];
+            }
+        }
+
+        return memo[0];        
     }
 };
